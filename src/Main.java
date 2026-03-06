038ad79e3837e9f135a01f0664daf8aa0b2a2075
VexVinny <viniciusdesousa220@gmail.com>
2026-03-05 22:09:20 -0300

    soma e multiplicação

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    static int soma(int a, int b){
+        return(a+b);
+    }
+    static int multi(int a, int b){
+        return(a*b);
+    }
+    static void main(){
+
+    }
+}
